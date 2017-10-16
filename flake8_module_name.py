b5f2ca0e32fa121a212ed8c6b40d9926ed80a592
Markus Quade <markus.quade@gmail.com>
2017-10-16 16:40:22 +0200

    init

---
+++ flake8_module_name.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+__version__ = "0.1.0"
+
+PATTERN = "[^a-z_]"
+
+search = re.compile(PATTERN).search
+
+def valid_pep8_filename(fname):
+    return not bool(search(fname))
+
+
+class FileNameChecker(object):
+    """Checker of PEP-8 Module Name Conventions."""
+    name = "module-naming"
+    version = __version__
+
+    def __init__(self, tree, filename):
+        self.filename = filename
+
+    def run(self):
+        base_name = os.path.basename(self.filename)
+        fn, _ = os.path.splitext(base_name)
+        if fn == "__init__":
+            fn = self.filename.split(os.path.sep)[-2]
+            typ = "modul"
+        else:
+            typ = "file"
+        valid = valid_pep8_filename(fn)
+        if not valid:
+            message = "N999 {}name should be all lower case".format(typ)
+            yield 0, 0, message, type(self)
