8b77ce4188912c3788f8837c3bdb66c288d853fd
Markus Quade <markus.quade@gmail.com>
2017-10-16 16:42:41 +0200

    init

--- setup.py
+++ setup.py
@@ -7,27 +7,27 @@ from setuptools import find_packages, setup, Command
 
 NAME = "flake8_module_name"
 DESCRIPTION = ""
-URL = 'https://github.com/ohjeah/flake8_module_name'
+URL = "https://github.com/ohjeah/flake8_module_name"
 EMAIL = "info@markusqua.de"
 AUTHOR = "Markus Quade"
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, "flake8_module_name.py")) as f:
     exec(f.read(), about)
 
 
 class PublishCommand(Command):
     """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -37,23 +37,23 @@ class PublishCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds ...')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds ...")
+            rmtree(os.path.join(here, "dist"))
         except FileNotFoundError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution...")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine...')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine...")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
 
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     author=AUTHOR,
@@ -61,16 +61,16 @@ setup(
     url=URL,
     packages=find_packages(exclude=["test", "example"]),
     install_requires=[],
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     cmdclass={
-        'publish': PublishCommand,
+        "publish": PublishCommand,
     },
     entry_points={
-        'flake8.extension': ['N999 = flake8_module_name:FileNameChecker'],
+        "flake8.extension": ["N999 = flake8_module_name:FileNameChecker"],
     },
 )
